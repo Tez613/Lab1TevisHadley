8b3a1ee084c16738fd0fe00a95ba003beac98ff7
Tez613 <tevishadley@gmail.com>
2022-09-08 16:39:58 -0600

    finished step 5

--- Program.cs
+++ Program.cs
@@ -16,6 +16,9 @@ Console.WriteLine(c);
 c=a/b;
 Console.WriteLine(c);
 }
+
+void OrderPrecedence()
+{
 int a=5;
 int b=4;
 int c=2;
@@ -30,3 +33,20 @@ int f=4;
 int g=3;
 int h=(e+f)/g;
 Console.WriteLine(h);
+}
+
+void Testlimits()
+{
+int a=7;
+int b=4;
+int c=3;
+int d=(a+b)/c;
+int e=(a+b)%c;
+Console.WriteLine($"quotient: {d}");
+Console.WriteLine($"remainder: {e}");
+int max= int.MaxValue;
+int min= int.MinValue;
+Console.WriteLine($"The range of integers is {min} to {max}");
+int what=max+3;
+Console.WriteLine($"An example of overflow: {what}");
+}
